f284c791e2c6fcde70a96f262a6977166a91df13
Rishvanth-Kumaresan <102664560+Rishvanth-Kumaresan@users.noreply.github.com>
2024-01-02 19:14:19 +0530

    Added sample.

---
+++ MaskCreditCardNumber/MaskCreditCardNumber/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MaskCreditCardNumber;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MaskCreditCardNumber/MaskCreditCardNumber/Sample/Behavior/DataFormBehavior.cs
@@ -0,0 +1,37 @@
+﻿using Syncfusion.Maui.DataForm;
+
+namespace MaskCreditCardNumber
+{
+    public class DataFormBehavior : Behavior<ContentPage>
+    {
+        private SfDataForm dataForm;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.dataForm = bindable.FindByName<SfDataForm>("dataForm");
+            this.dataForm.GenerateDataFormItem += OnGenerateDataFormItem;    
+        }
+        private void OnGenerateDataFormItem(object sender, GenerateDataFormItemEventArgs e)
+        {
+            if(e.DataFormItem != null)
+        {
+                if (e.DataFormItem.FieldName == "CardNumber")
+                {
+                    var maskedItem = e.DataFormItem as DataFormMaskedTextItem;
+                    maskedItem.Mask = "0000-0000-0000-0000";
+                    maskedItem.PromptChar = 'X';
+                    maskedItem.Keyboard = Keyboard.Numeric;
+                }
+            }
+        }
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            if (this.dataForm != null)
+            {
+                this.dataForm.GenerateDataFormItem -= OnGenerateDataFormItem;
+                this.dataForm = null;
+            }
+        }
+    }
+}
