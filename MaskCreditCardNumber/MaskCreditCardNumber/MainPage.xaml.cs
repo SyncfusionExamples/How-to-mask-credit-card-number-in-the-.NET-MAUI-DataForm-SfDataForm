a518981ed2981a924f57366b7153fbe87258ded1
Rishvanth-Kumaresan <102664560+Rishvanth-Kumaresan@users.noreply.github.com>
2024-01-04 13:10:44 +0530

    Sample updated.

--- MaskCreditCardNumber/MaskCreditCardNumber/MainPage.xaml.cs
+++ MaskCreditCardNumber/MaskCreditCardNumber/MainPage.xaml.cs
@@ -1,9 +1,25 @@
-﻿namespace MaskCreditCardNumber;
+﻿using Syncfusion.Maui.DataForm;
+
+namespace MaskCreditCardNumber;
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+        dataForm.GenerateDataFormItem += OnGenerateDataFormItem;
+    }
+    private void OnGenerateDataFormItem(object sender, GenerateDataFormItemEventArgs e)
+    {
+        if (e.DataFormItem != null)
+        {
+            if (e.DataFormItem.FieldName == "CardNumber")
+            {
+                var maskedItem = e.DataFormItem as DataFormMaskedTextItem;
+                maskedItem.Mask = "0000-0000-0000-0000";
+                maskedItem.PromptChar = 'X';
+                maskedItem.Keyboard = Keyboard.Numeric;
+            }
+        }
+    }
 }
